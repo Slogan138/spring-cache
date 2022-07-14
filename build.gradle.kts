ea428fc5ae5766bdf996766eb6cfde12a9012f4e
박진우 <slogan138@gmail.com>
2022-07-15 08:38:37 +0900

    test: kotest 테스트 케이스 초안 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/io/slogan/cache/webflux/GetTests.kt
@@ -0,0 +1,25 @@
+package io.slogan.cache.webflux
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
+import io.slogan.cache.webflux.controller.DataController
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(
+    classes = [WebfluxApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@AutoConfigureWebTestClient
+internal class GetTests(
+    val dataController: DataController,
+) : DescribeSpec() {
+    override fun extensions() = listOf(SpringExtension)
+
+    init {
+        this.describe("Get Value By `hello` then Return `world`") {
+            val input = "hello"
+            dataController.get(input).body?.block() shouldBe "world"
+        }
+    }
+}
